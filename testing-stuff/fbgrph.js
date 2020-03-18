44571f2debf593e1447227878e4a35e2e0e9583e
adityachanekar <chanekar.aditya@gmail.com>
2020-03-18 14:47:12 +0530

    working post file

--- testing-stuff/fbgrph.js
+++ testing-stuff/fbgrph.js
@@ -1,12 +1,12 @@
 const graph = require("fbgraph");
 
-let appId = `211761870180278`;
-let secret = `d34304d06b20598e6090ecf7603576be`;
-let access_token = `EAADAmKcMj7YBABfJ60JxxddtleBq2TX0CKEZB2d6iTTO1yZAz5dsAvDbSOceZBky34F2GVT38gpHr7EMCffwwoRezEUfQiOG6BUuMnXrzA0ZB49j2wcXGrt6uBHVlhEFeGXjzbk63iQAr1L67amR2Pw81tW8A5htLR5LCDC7DQWgb9743BsQgxO1evgjlFZCFZCkk9aHB29uFSov0ifIckxFF4BvzFMrZBnfF8xMf9jrwZDZD`;
+let appId = '211761870180278';
+let secret = 'd34304d06b20598e6090ecf7603576be';
+let access_token = 'EAADAmKcMj7YBANZB5dGCWozdba6t6Kh4S8YXzvK1EImGjfZBbnA4VAQpN5vHDrMOJ5j1g0Yx0CHATz9ZCNqFTUFeT682bor4JVG1ZAr3yrSyyXaQDe9ZAYV3XfnZCX48tIPhE3MphnwFkwCrV2JQMK4qRF93ycKmlwxmJZBZCZCXlNx8iJXJK9ARVazXTueLnA3zW3iN4IBpAjZAjlYmi4ZB6NowH63tb5f3OAXKGJyjFPwvQZDZD';
 let userId = "3492229184147401";
 graph.setAccessToken(access_token);
 let wallPost = {
-    "message": "messsage"
+    "message": "Testing from script"
 };
 
 graph.post(userId + "/feed?access_token=" + access_token, wallPost, function(err, res) {
