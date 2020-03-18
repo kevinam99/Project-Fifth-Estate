c720b3a1ab469894c8b712414612111250ff644f
adityachanekar <chanekar.aditya@gmail.com>
2020-03-18 17:50:02 +0530

    Trying to save response into JSON file using Dummy Group

--- testing-stuff/fbgrph.js
+++ testing-stuff/fbgrph.js
@@ -5,8 +5,13 @@ let secret = 'd34304d06b20598e6090ecf7603576be';
 let access_token = 'EAADAmKcMj7YBANZB5dGCWozdba6t6Kh4S8YXzvK1EImGjfZBbnA4VAQpN5vHDrMOJ5j1g0Yx0CHATz9ZCNqFTUFeT682bor4JVG1ZAr3yrSyyXaQDe9ZAYV3XfnZCX48tIPhE3MphnwFkwCrV2JQMK4qRF93ycKmlwxmJZBZCZCXlNx8iJXJK9ARVazXTueLnA3zW3iN4IBpAjZAjlYmi4ZB6NowH63tb5f3OAXKGJyjFPwvQZDZD';
 let userId = "3492229184147401";
 graph.setAccessToken(access_token);
+
+
 let wallPost = {
-    "message": "Testing from script"
+    
+    "message": "Aditya Here Testing again from Rome this time \n #iLoveKORUNA",
+    
+    
 };
 
 graph.post(userId + "/feed?access_token=" + access_token, wallPost, function(err, res) {
--- testing-stuff/goaspeaks.js
+++ testing-stuff/goaspeaks.js
@@ -3,7 +3,8 @@ const graph = require("fbgraph");
 let appId = '211761870180278';
 let secret = 'd34304d06b20598e6090ecf7603576be';
 let access_token = 'EAADAmKcMj7YBANZB5dGCWozdba6t6Kh4S8YXzvK1EImGjfZBbnA4VAQpN5vHDrMOJ5j1g0Yx0CHATz9ZCNqFTUFeT682bor4JVG1ZAr3yrSyyXaQDe9ZAYV3XfnZCX48tIPhE3MphnwFkwCrV2JQMK4qRF93ycKmlwxmJZBZCZCXlNx8iJXJK9ARVazXTueLnA3zW3iN4IBpAjZAjlYmi4ZB6NowH63tb5f3OAXKGJyjFPwvQZDZD';
-let userId = "164559246924824/feed"; ///goa speaks
+//let userId = "164559246924824/feed"; ///goa speaks
+let userId = "3492229184147401/feed?fields=place,message,picture,message_tags&limit(1000)";
 graph.setAccessToken(access_token);
 let wallPost = {
     "message": "Testing from script"
@@ -14,13 +15,19 @@ var options = {
   , pool:     { maxSockets:  Infinity }
   , headers:  { connection:  "keep-alive" }
 };
- 
+const fs = require('fs');
+let path = "./file.JSON";
 graph
   .setOptions(options)
   .get(userId, function(err, res) {
-    console.log(res); // { id: '4', name: 'Mark Zuckerberg'... }
+    //console.log(res); // { id: '4', name: 'Mark Zuckerberg'... }
+    
+const storeData = (res, path) => {
+    try {
+      fs.writeFile(path, JSON.stringify(res))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+    
   });
-// graph.post(userId + "/feed?access_token=" + access_token, wallPost, function(err, res) {
-//     // returns the post id
-//     console.log(res); // { id: xxxxx}
-// });
