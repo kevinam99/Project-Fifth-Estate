39aeaf08ca79fd1daf3e64e7856ad6546aa51f52
kevinam99 <kevinam99@gmail.com>
2020-03-19 21:55:58 +0530

    Getting more precise data

--- testing-stuff/goaspeaks.js
+++ testing-stuff/goaspeaks.js
@@ -2,7 +2,7 @@ const graph = require("fbgraph");
 
 let appId = '211761870180278';
 let secret = 'd34304d06b20598e6090ecf7603576be';
-let access_token = 'EAADAmKcMj7YBANZB5dGCWozdba6t6Kh4S8YXzvK1EImGjfZBbnA4VAQpN5vHDrMOJ5j1g0Yx0CHATz9ZCNqFTUFeT682bor4JVG1ZAr3yrSyyXaQDe9ZAYV3XfnZCX48tIPhE3MphnwFkwCrV2JQMK4qRF93ycKmlwxmJZBZCZCXlNx8iJXJK9ARVazXTueLnA3zW3iN4IBpAjZAjlYmi4ZB6NowH63tb5f3OAXKGJyjFPwvQZDZD';
+let access_token = 'EAADAmKcMj7YBAO2cHbk0g57VeVRcPaujHV7mLRqkCVArFcLhNZBZClNSYQQjEtTZBUxaVxzeLR0owfB9GjYEfhlZBGZBdk6cCcEZCkx7vUIk8HHmKfvEdsveBrIGEKlnt5R5AnQkXxPHdbeeBx5zqvRNypE71ZBx2PTDBhv0j2CoVugkFKv6BApoNlrdnQ1fNC4wjHZAacbyjj1Kr8N2ZBnSqb3055IDQ0tcZB0OM0uhqx1Gje0rEgd6cY';
 //let userId = "164559246924824/feed"; ///goa speaks
 let userId = "3492229184147401/feed?fields=place,message,picture,message_tags&limit(1000)";
 graph.setAccessToken(access_token);
@@ -21,7 +21,7 @@ let path = "./file.json";
 
 graph
   .setOptions(options)
-  .get("210553450180199/feed?fields=message,message_tags,story_tags", function(err, res) {
+  .get("210553450180199/feed?fields=description,full_picture,message,message_tags,story_tags,created_time,coordinates,name,link,place,picture,application,status_type,type,attachments{media,description}", function(err, res) {
     try {
         console.log(res);
         fs.writeFileSync(path, JSON.stringify(res, null, 4))
