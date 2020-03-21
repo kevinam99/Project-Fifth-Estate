45c6d55d5dfa979759d54764ec2a994e5639949a
kevinam99 <kevinam99@gmail.com>
2020-03-21 15:26:45 +0530

    Getting feed using fb module

--- testing-wth-node-sdk/getFeed.js
+++ testing-wth-node-sdk/getFeed.js
@@ -4,10 +4,10 @@ const secrets = require('../testing-stuff/secrets.json');
 FB.options({version: 'v6.0'});
 
 let app = FB.extend({appId: secrets.app.id, appSecret: secrets.app.secret})
-let access_token = secrets.page.access_token;
+let page_access_token = secrets.page.access_token;
 let groupId = secrets.groups.kevin;
 
-FB.setAccessToken(access_token);
+FB.setAccessToken(page_access_token);
 
 FB.api(`/${groupId}/feed`, 'GET', {"fields":"description,full_picture,message,message_tags,story_tags,created_time,coordinates,name,link,place,picture,status_type,type,attachments{media}"},
     function(res) {
@@ -19,6 +19,7 @@ FB.api(`/${groupId}/feed`, 'GET', {"fields":"description,full_picture,message,me
         console.log(res.data.length);
       // console.log(JSON.stringify(res.data[0], null, 4));
         try {
+          // writing feed to file
             const fs = require('fs');
             let path = "./file.json";
             fs.writeFileSync(path, JSON.stringify(res, null, 4))
