b748fdd6508f918105077fb0d91d169184e6d34a
kevinam99 <kevinam99@gmail.com>
2020-03-21 15:26:50 +0530

    Upload picture from local using fb module

--- testing-wth-node-sdk/uploadPic.js
+++ testing-wth-node-sdk/uploadPic.js
@@ -1,3 +1,4 @@
+// Uploading pic as the page
 const FB = require('fb').default;
 const secrets = require('../testing-stuff/secrets.json');
 const fs = require('fs');
@@ -5,12 +6,12 @@ const fs = require('fs');
 FB.options({version: 'v6.0'});
 
 let app = FB.extend({appId: secrets.app.id, appSecret: secrets.app.secret})
-let access_token = secrets.page.access_token;
+let page_access_token = secrets.page.access_token;
 let groupId = secrets.groups.kevin;
 
-FB.setAccessToken(access_token);
+FB.setAccessToken(page_access_token);
 
-let imgPath = '/home/kevin/Pictures/Selection_015.png';
+let imgPath = '/home/kevin/Pictures/Selection_015.png'; // image from local machine
 let imgCaption = 'Trying to upload using the SDK module';
 
 FB.api(`${groupId}/photos`, 'post', { 
@@ -19,7 +20,7 @@ FB.api(`${groupId}/photos`, 'post', {
     alt_text_custom: `P5E weekly report`,
     place: "693912274113140"
     }
-    , function (res) {
+    ,function (res) {
   if(!res || res.error) {
     console.log(!res ? 'error occurred' : res.error);
     return;
