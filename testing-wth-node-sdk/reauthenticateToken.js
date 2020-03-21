fb59af6ae51e098bab48e4c4d3353c44563081c7
kevinam99 <kevinam99@gmail.com>
2020-03-21 21:33:35 +0530

    Token reauth complete!

--- testing-wth-node-sdk/reauthenticateToken.js
+++ testing-wth-node-sdk/reauthenticateToken.js
@@ -11,18 +11,27 @@ function getLongLivedUserAccessToken()
     let options = {
         client_id: secrets.app.id,
         client_secret: secrets.app.secret,
-        fb_exchange_token: 'EAADAmKcMj7YBAHHG6CcTegDgjz3mAI0glVA5szBiIxnzZAnEaB8FOo776HmksVKHMVJ9oZBt0FhcLQ0yLwtZBegg0wPOmNWKi8xZBDZBWtVcVQfDFPOoZAjuBTP2NLuijj2ZA1dYDefCB5QsqdgAl4AjglB7S9mcWnPp3oHFgiyVvqaDmZBmmVI90nUbucm1Cy93OooCFGhhQJZBvVz24s6WW',
+        fb_exchange_token: secrets.user.kevin,
         grant_type: 'fb_exchange_token'
     }
 
-    FB.api('oauth/access_token', options, function (res) {
+    FB.api('/oauth/access_token', options, function (res) {
         if(!res || res.error) {
             console.error(!res ? 'error occurred' : res.error);
             return;
         }
     
         var accessToken = res.access_token;
-        console.log(accessToken);
+        console.log(`User Access token: ${accessToken}\n\n`);
+        try {
+            // writing feed to file
+              const fs = require('fs');
+              let path = "./longLivedUserAccessToken.json";
+              fs.writeFileSync(path, JSON.stringify(res, null, 4))
+            } 
+          catch (err){
+            console.error(err)
+          }
         getLongLivedPageAccessToken(accessToken);
     });
 }
@@ -39,16 +48,27 @@ function getLongLivedPageAccessToken(userAccessToken)
     access_token={user-access-token}`;
 
     let options = {
+        //fields: secrets.page.access_token,
+        fields: "access_token",
         access_token: userAccessToken
     }
     const pageId= secrets.page.id;
-    FB.api(pageId +`?fields=access_token`, options, function (res) {
+    FB.api('/' + pageId, 'GET', options, function (res) {
         if(!res || res.error) {
             console.error(!res ? 'error occurred' : res.error);
             return;
         }
         else{
             console.log(res.access_token);
+            try {
+                // writing feed to file
+                  const fs = require('fs');
+                  let path = "./longLivedPageAccessToken.json";
+                  fs.writeFileSync(path, JSON.stringify(res, null, 4))
+                } 
+              catch (err){
+                console.error(err)
+              }
         }
     
     });
