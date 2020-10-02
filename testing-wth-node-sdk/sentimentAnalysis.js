5c7c8659621cab7c942535b532ce9ea038ecb5d8
kevinam99 <kevinam99@gmail.com>
2020-10-02 14:20:34 +0530

    mk

--- testing-wth-node-sdk/sentimentAnalysis.js
+++ testing-wth-node-sdk/sentimentAnalysis.js
@@ -4,6 +4,7 @@ let complaints = ['why has The electricity has been down for an hour.',
              'the municipality has not collected the trash',            
 ];
 
+// Add complaints to the array and check the output
 complaints.forEach(complaint => {
     const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(complaint);
     console.log(`${complaint}\n${JSON.stringify(intensity)}\n\n`);
