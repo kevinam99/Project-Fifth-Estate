5a658f8b8c52a2872fb22447758c24566a327d48
kevinam99 <kevinam99@gmail.com>
2020-10-02 14:52:56 +0530

    Added method to validate webhook

--- testing-wth-node-sdk/webhook.js
+++ testing-wth-node-sdk/webhook.js
@@ -8,13 +8,6 @@ FB.options({version: 'v6.0'});
 const port = process.env.PORT || 5000
 express_app.use(bodyParser.urlencoded({ extended: false }));
 express_app.use(bodyParser.json());
-express_app.get('/', (req, res) => {
-  res.send("Hello! Welcome to 5E")
-})
-
-express_app.post('/', (req, res) => {
-	res.send("Webhook received")
-})
 
 let app = FB.extend({appId: secrets.app.id, appSecret: secrets.app.secret})
 let groupId = secrets.groups.kevin;
@@ -24,7 +17,7 @@ FB.setAccessToken(appAccessToken)// must use app access token only
 
 const setWebhook = (callback_url, fields) => {
 	FB.api(`/${secrets.app.id}/subscriptions`, 'POST', {
-		object: `user`, // group to which you want to subscribe
+		object: `group`, // type pf subscription
 		callback_url: callback_url, // change this with your own ngrok url
 		fields: fields,
 		// fields: `description,full_picture,message,message_tags,story_tags,created_time,coordinates,name,link,place,picture,status_type,type,attachments{media}`, // stuff that you want to access
@@ -62,8 +55,17 @@ const deleteWebhook = fields => {
 }
 
 let callback_url = `https://98ae67c300f3.ngrok.io` // must use https only
-let fields = `photos, feed`
+let fields = `posts`
 setWebhook(callback_url, fields)
 // deleteWebhook(fields)
 
 // You can manage your webhooks here: https://developers.facebook.com/apps/211761870180278/webhooks/
+
+express_app.listen(port, () =>{
+	console.log(`Listening on port ${port}`)
+})
+
+express_app.get('/', (req, res) => {
+	console.log(req.query['hub.challenge'])
+	res.send(req.query['hub.challenge'])
+})
