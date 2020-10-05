7d7e2611c010a01fede593f43fc62ff104984a16
kevinam99 <kevinam99@gmail.com>
2020-10-05 15:59:01 +0530

    Segregate based on departments

--- testing-wth-node-sdk/segregatePosts.js
+++ testing-wth-node-sdk/segregatePosts.js
@@ -1,15 +1,36 @@
 const fs = require('fs')
 
 
-let path = "./file.json"
+const path = "./file.json"
 
 
 const data = fs.readFileSync(path)
 const posts = JSON.parse(data)
 
-var hastagPosts = posts.data.filter(e => 'message_tags' in e)
-var results = hastagPosts.filter(e => e.message_tags.find(items => {
+// segregating based on #greg
+const hastagPosts = posts.data.filter(e => 'message_tags' in e)
+const gregPosts = hastagPosts.filter(e => e.message_tags.find(items => {
     
     return items.name.toLowerCase() == "#greg"
 }))
-console.log(results)
+console.log(gregPosts) // greg
+
+
+// segregating based on departments
+
+const departments = {
+                        goapwd: true, 
+                        goaelectricity: true, 
+                        goamunicipality: true, 
+                        goatransport: true
+                    }
+
+gregPosts.forEach(post => {
+    post.message_tags.find(items => {
+        const dept = items.name.toLowerCase().split('#')[1]
+        if(departments[dept]) // if dept exists
+        {
+            console.log(`Complaint => [ ${post.message} ]\ndepartment => ${dept}`)
+        }
+    })
+})
