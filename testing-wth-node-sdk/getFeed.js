faec4a7bb3038941088b6b7c0b46f22ed30a62c3
kevinam99 <kevinam99@gmail.com>
2020-10-08 21:25:08 +0530

    Checking comments for #greg

--- testing-wth-node-sdk/getFeed.js
+++ testing-wth-node-sdk/getFeed.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const FB = require('fb').default;
-const secrets = require('../testing-stuff/secrets.json');
+// const secrets = require('../testing-stuff/secrets.json')
 const express_app = require('express')()
-const bodyParser = require('body-parser');
-FB.options({version: 'v6.0'});
+const bodyParser = require('body-parser')
+FB.options({version: process.env.API_VERSION})
 
 const port = process.env.PORT || 5000
 express_app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,16 +12,17 @@ express_app.use(bodyParser.json());
 
 express_app.post('/', (req, res) => {
   res.send("Webhook received")
-  console.log(JSON.stringify(req.body.entry))
+  console.log(JSON.stringify(req.body.entry, null, 4))
+  console.log(`IP: ${req.ip}`)
 })
 
-let app = FB.extend({appId: secrets.app.id, appSecret: secrets.app.secret})
-let page_access_token = secrets.page.access_token;
-let groupId = secrets.groups.kevin;
+FB.extend({appId: process.env.APP_ID, appSecret: process.env.APP_SECRET})
+let page_access_token = process.env.PAGE_ACCESS_TOKEN
+let groupId = 210553450180199
 
 FB.setAccessToken(page_access_token);
 
-FB.api(`/${groupId}/feed`, 'GET', {"fields":"description,full_picture,message,message_tags,story_tags,created_time,coordinates,name,link,place,picture,status_type,type,attachments{media}"},
+FB.api(`/${groupId}/feed`, 'GET', {"fields":"description,full_picture,message,message_tags,story_tags,created_time,coordinates,name,link,place,picture,status_type,type,attachments{media},comments{message_tags}"},
     function(res) {
       if(res.error)
       {
