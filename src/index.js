475de2e1efb67e7321c00c67acff8ef958a200de
adityachanekar <chanekar.aditya@gmail.com>
2020-12-09 15:33:02 +0530

    added https to fb links, and enabled cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config("./.env");
 const FB = require("fb").default;
 const app = require("express")();
 const bodyParser = require("body-parser");
+const cors = require('cors')
 
 const getFeed = require("./services/getFeed");
 const { filterGregPosts, segregate, createNewTag } = require("./services/segregatePosts");
@@ -11,6 +12,7 @@ const logger = require("./logger/logger");
 const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 FB.options({ version: process.env.API_VERSION });
 FB.extend({ appId: process.env.APP_ID, appSecret: process.env.APP_SECRET });
@@ -87,3 +89,49 @@ app.patch("/hashtag", (req, res) => {
 		res.sendStatus(500);
 	}
 });
+
+app.get("/dbposts", async(req, res) => {
+	console.log("Fetching Issues from DB")
+	try {
+		const issues = await fetchPosts("2020-10-18");
+		console.log(issues);
+		res.send(issues);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+app.get("/posts", async(req,res)=>{
+	try {
+		const posts = await getFeed();
+		logger.info(`(index.js)...posts.length = ${posts.length}.`);
+
+		if (posts.length > 0) {
+			const filteredGregPosts = await filterGregPosts(posts);
+			logger.info(
+				`(index.js)... greg posts = ${filteredGregPosts.length}`
+			);
+			// console.log(
+			// 	`greg posts (from index.js) = ${filteredGregPosts.length}`
+			// );
+			// console.log(filteredGregPosts)
+			logger.info("(index.js)... Segregating Posts");
+			// console.log("Segregating Posts");
+
+			const segregatedPosts = await segregate(filteredGregPosts); //segregates posts and returns an array of obj containing all the posts
+
+			logger.info("(index.js)... Storing segregated posts to db");
+			//console.log("storing segregated posts to db");
+			
+			const saved = await storePosts(segregatedPosts); //the array of posts is stored to the db
+			logger.info(`(index.js)... Posts saved to DB`);
+			res.send(`Saved ${saved}...(index.js)`+ `\r\n${JSON.stringify(segregatedPosts,null,4)}`);
+		} else {
+			logger.info(`(index.js)... No posts available at this time`);
+			res.send(`No posts available at this time...(index.js)`);
+		}
+	} catch (err) {
+		logger.error(`(index.js)... Error: ${err}`);
+		res.send(err);
+	}
+})
