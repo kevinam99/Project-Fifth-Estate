1070efb9ee2374756b73c29390471f8ccd358e90
Aditya <chanekar.aditya@gmail.com>
2021-04-06 14:37:34 +0530

    contents of main shifted inside api call

--- src/index.js
+++ src/index.js
@@ -26,30 +26,30 @@ app.post('/', (req, res) => {
 	res.sendStatus(200)
 })
 
-const main = async () => {
-	try {
-		const posts = await getFeed()
-		logger.info(`(index.js)...posts.length = ${posts.length}.`)
-		if (posts.length > 0) {
-			// const filteredGregPosts = await filterGregPosts(posts)
-			// logger.info(`(index.js)... greg posts = ${filteredGregPosts.length}`)
-			// console.log(filteredGregPosts)
-			logger.info("(index.js)... Segregating Posts")
-			const segregatedPosts = await segregate(posts); //segregates posts and returns an array of obj containing all the posts
+// const main = async () => {
+// 	try {
+// 		const posts = await getFeed()
+// 		logger.info(`(index.js)...posts.length = ${posts.length}.`)
+// 		if (posts.length > 0) {
+// 			// const filteredGregPosts = await filterGregPosts(posts)
+// 			// logger.info(`(index.js)... greg posts = ${filteredGregPosts.length}`)
+// 			// console.log(filteredGregPosts)
+// 			logger.info("(index.js)... Segregating Posts")
+// 			const segregatedPosts = await segregate(posts); //segregates posts and returns an array of obj containing all the posts
 			
-			logger.info("(index.js)... Storing segregated posts to db")
-			const saved = await storePosts(segregatedPosts) //the array of posts is stored to the db
-			if(saved) logger.info(`(index.js)... Posts saved to DB`);
+// 			logger.info("(index.js)... Storing segregated posts to db")
+// 			const saved = await storePosts(segregatedPosts) //the array of posts is stored to the db
+// 			if(saved) logger.info(`(index.js)... Posts saved to DB`);
 			
-		} else {
-			logger.info(`(index.js)... No posts available at this time`)
-		}
-	} catch (err) {
-		logger.error(`(index.js)... Error: ${err}`)
-	}
-};
+// 		} else {
+// 			logger.info(`(index.js)... No posts available at this time`)
+// 		}
+// 	} catch (err) {
+// 		logger.error(`(index.js)... Error: ${err}`)
+// 	}
+// };
 
-main();
+// main();
 // posts.then(res => { logger.info(`posts.length (from index.js) = ${res}`)})
 // posts.catch(err => console.error(err))
 
@@ -111,17 +111,10 @@ app.get("/posts", async(req,res)=>{
 			logger.info(
 				`(index.js)... greg posts = ${filteredGregPosts.length}`
 			);
-			// console.log(
-			// 	`greg posts (from index.js) = ${filteredGregPosts.length}`
-			// );
-			// console.log(filteredGregPosts)
 			logger.info("(index.js)... Segregating Posts");
-			// console.log("Segregating Posts");
-
 			const segregatedPosts = await segregate(filteredGregPosts); //segregates posts and returns an array of obj containing all the posts
 
 			logger.info("(index.js)... Storing segregated posts to db");
-			//console.log("storing segregated posts to db");
 			
 			const saved = await storePosts(segregatedPosts); //the array of posts is stored to the db
 			logger.info(`(index.js)... Posts saved to DB`);
