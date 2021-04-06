8546be8f782cecb9c052aac58e57ed45d1171064
Aditya <chanekar.aditya@gmail.com>
2021-04-06 14:47:27 +0530

    removed main from index.js

--- src/index.js
+++ src/index.js
@@ -26,49 +26,7 @@ app.post('/', (req, res) => {
 	res.sendStatus(200)
 })
 
-// const main = async () => {
-// 	try {
-// 		const posts = await getFeed()
-// 		logger.info(`(index.js)...posts.length = ${posts.length}.`)
-// 		if (posts.length > 0) {
-// 			// const filteredGregPosts = await filterGregPosts(posts)
-// 			// logger.info(`(index.js)... greg posts = ${filteredGregPosts.length}`)
-// 			// console.log(filteredGregPosts)
-// 			logger.info("(index.js)... Segregating Posts")
-// 			const segregatedPosts = await segregate(posts); //segregates posts and returns an array of obj containing all the posts
-			
-// 			logger.info("(index.js)... Storing segregated posts to db")
-// 			const saved = await storePosts(segregatedPosts) //the array of posts is stored to the db
-// 			if(saved) logger.info(`(index.js)... Posts saved to DB`);
-			
-// 		} else {
-// 			logger.info(`(index.js)... No posts available at this time`)
-// 		}
-// 	} catch (err) {
-// 		logger.error(`(index.js)... Error: ${err}`)
-// 	}
-// };
 
-// main();
-// posts.then(res => { logger.info(`posts.length (from index.js) = ${res}`)})
-// posts.catch(err => console.error(err))
-
-// const gregPosts = Controller.greg()
-// console.log(`greg posts (from index.js) = ${gregPosts.length}`)
-
-// if(gregPosts.length > 0)
-// {
-//     const {
-//         link,
-//         complaint,
-//         dept,
-//         place,
-//         time,
-//         date
-//     } = Controller.segregate()
-//     console.log("In if block")
-
-// }
 
 app.post("/complaint", (req, res) => {
 	res.sendStatus(200);
