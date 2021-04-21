00ae6186e68c0aa8977e9782681e6c6b49cd6a25
kevinam99 <kevinam99@gmail.com>
2021-04-21 12:10:11 +0530

    Add fetchPosts()

--- src/services/dbConnect.js
+++ src/services/dbConnect.js
@@ -3,44 +3,30 @@ const mongoose = require('mongoose')
 const Complaint = require('../models/complaint.model')
 const logger = require('../logger/logger')
 
+const MONGODB_URI = process.env.MONGODB_URI
 //db connection
 const storePosts = async (segregatedPosts) => {
-	const mongo_uri = `mongodb+srv://greg:${process.env.MONGO_PASSWORD}@cluster0.adgjc.mongodb.net/demo?retryWrites=true&w=majority`;
-	try {
-		await mongoose.connect(mongo_uri, {
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			useUnifiedTopology: true,
-		})
-		logger.info("(dbConnect.js)... Mongo connected")
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
 
-		await Complaint.insertMany(segregatedPosts)
-		logger.info(`(dbConnect.js) line 24... Complaints saved to DB.`)
-	}
-	
-	catch(error) {
-		logger.error(`(dbConnect.js) line 19... ${error}`)
-	}
-	// const connection = mongoose.connection;
+    await Complaint.insertMany(segregatedPosts)
+    logger.info(`(dbConnect.js)... Complaints saved to DB.`)
+  } catch (error) {
+    logger.error(`(dbConnect.js) line 19... ${error}`)
+  }
+  return 1
+}
+const fetchPosts = async (date) => {
+  mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
 
-	// connection
-	// 	.once("open", () => {
-	// 		//connected {db operations inside this method only}
-	// 		console.log("Connected to MongoDB!");
-	// 		let issue = new Complaint(segregatedPosts);
-
-	// 		issue.save((err, issue) => {
-	// 			if (err) return console.error(err);
-
-	// 			console.log(issue);
-	// 		});
-	// 	})
-	// 	.catch((error) => {
-	// 		// handling a rejected promise
-	// 		console.log("Mongo not connected");
-	// 		console.error(error);
-	// 	});
-
-};
-
-module.exports = storePosts;
+  return Complaint.find({})
+}
+module.exports = { storePosts, fetchPosts }
