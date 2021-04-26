ce84ad04cd802cc3ef521506ab6f1bcfd22e1c0d
kevinam99 <kevinam99@gmail.com>
2021-04-26 22:13:45 +0530

    Add connect() method for connecting once.

--- src/services/dbConnect.js
+++ src/services/dbConnect.js
@@ -4,36 +4,43 @@ const Complaint = require('../models/complaint.model')
 const logger = require('../logger/logger')
 
 const MONGODB_URI = process.env.MONGODB_URI
-//db connection
-const storePosts = async (segregatedPosts) => {
+
+const connectDB = async(MONGODB_URI) => {
   try {
     await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
     })
-
-    await Complaint.insertMany(segregatedPosts)
-    logger.info(`(dbConnect.js)... Complaints saved to DB.`)
+    logger.info("Connected to Mongo Atlas")
+  }
+  catch(error) {
+    logger.error(`Error while connecting to Mongo Atlas: ${error}`)
+  }
+}
+//db connection
+const storePosts = async (segregatedPosts) => {
+    try{
+      await Complaint.insertMany(segregatedPosts)
+      logger.info(`(dbConnect.js)... Complaints saved to DB.`)
+    
   } catch (error) {
-    logger.error(`(dbConnect.js) line 19... ${error}`)
+    logger.error(`(dbConnect.js) line 27... ${error}`)
+    return error
   }
   return 1
 }
 const fetchPosts = async (date) => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    })
-    
     return Complaint.find({})
     
   } catch (error) {
-    logger.error(`(dbConnect.js) line 31... ${error}`)
+    logger.error(`(dbConnect.js) line 37... ${error}`)
+    return error
   }
 
   
 }
+
+connectDB(MONGODB_URI)
 module.exports = { storePosts, fetchPosts }
