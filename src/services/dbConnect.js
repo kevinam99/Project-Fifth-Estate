1738c805c2520e75aaf2a9363ea1e0a4c59db244
Kevin <kevinam99@gmail.com>
2021-05-07 14:43:17 +0530

    Migrate to Firebase Realtime DB
    Completed the migration of saving all the posts to Firebase Realtime DB.
    The workings are the same at high level.
    
    Internally, we're running the save posts query for every single post in a batch.
    This is because, each save query saves a record with a unique key. That means,
    when a bunch posts in a list are saved using one save query, the posts get saved
    as nested records under one uniue key, which serves as the ID for each record.
    
    To test for now, just call the functions in src/services/dbConnect.js
    
    Be sure to take the service account credentials from me. The Firebase console
    generates keys only once and it will be a problem if the keys are changed constantly.
    The .env is also updated to have the URL to the DB.
    
    Install the new dependencies.

--- src/services/dbConnect.js
+++ src/services/dbConnect.js
@@ -1,67 +1,73 @@
 /** @module dbConnect */
-require('dotenv').config()
-const mongoose = require('mongoose')
-const Complaint = require('../models/complaint.model')
-const logger = require('../logger/logger')
+require("dotenv").config()
+const admin = require("firebase-admin");
+const serviceAccount = require("../../project-5e-firebase-adminsdk-wk2m9-3ef3f96fa7.json");
+const logger = require("../logger/logger");
 
-const MONGODB_URI = process.env.MONGODB_URI
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: process.env.FIREBASE_URL
+  });
+logger.info("Initialized Firebase Realtime DB app!")
 
-/**
- * This function helps to connect to MongoDB Cloud Atlas before any post is stored.
- *
- * @param {string} MONGODB_URI - The URI to connect to MongoDB Cloud Atlas.
- * @returns {string} response - The response from MongoDB Cloud Atlas.
- */
-const connectDB = async(MONGODB_URI) => {
-  try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    })
-    logger.info("Connected to Mongo Atlas!")
-  }
-  catch(error) {
-    logger.error(`Error while connecting to Mongo Atlas: ${error}`)
-  }
-}
+
+
 /** 
  * This fuction stores the posts to the DB.
  * @param {object} segregatedPosts - Contains the relevant posts with #greg along with the department and sentiment of the post.
  * 
  */
 const storePosts = async (segregatedPosts) => {
-    try{
-      await connectDB(MONGODB_URI)
-      await Complaint.insertMany(segregatedPosts)
-      logger.info(`(dbConnect.js)... Complaints saved to DB.`)
-    
-  } catch (error) {
-    logger.error(`(dbConnect.js)... ${error}`)
-    return error
-  }
-  return 1
+		try{
+		
+			const ref = admin.database().ref("posts")
+			segregatedPosts.forEach(segregatedPost => {
+				const newPostRef = ref.push()
+				newPostRef.set(segregatedPost, error => {
+					if (error) {
+						// The write failed...
+						logger.error("(dbConnect.js)... Error while adding post: " + error)
+					} else {
+						// The write was successful...
+						logger.info(`(dbConnect.js)... Complaints saved to DB.`)
+						
+					}
+				})
+				
+			});
+			
+		
+	} catch (error) {
+		logger.error(`(dbConnect.js)... ${error}`)
+		return error
+	}
+	return 1
 }
 
 /**
  * 
  * This function fetches posts from the DB with the given date.
- * @param {string} date - Posts originationg from the specified date.
+ * @param {string} date - Posts originating from the specified date.
  * 
- * @returns {object} Complaint - Contains all the complaints of the given date.
+ * @returns {object} data - Contains all the complaints of the given date.
  */
 const fetchPosts = async (date) => {
-  try {
-    await connectDB(MONGODB_URI)
-    return Complaint.find({})
-    
-  } catch (error) {
-    logger.error(`(dbConnect.js)... ${error}`)
-    return error
-  }
-
-  
+	try {
+		const ref = admin.database().ref("posts")
+		let data
+		if(date) data =  await ref.orderByChild("date").equalTo(date).once("value")
+		else if (!date) data =  await ref.once("value")
+		
+		ref.off("value");
+		if(process.env.NODE_ENV == "dev") console.log(data.val())
+		return data.val()
+		
+		
+	} catch (error) {
+		logger.error(`(dbConnect.js)... Error file fetching posts:  ${error}`)
+		return error
+	}
 }
 
-
+fetchPosts()
 module.exports = { storePosts, fetchPosts }
